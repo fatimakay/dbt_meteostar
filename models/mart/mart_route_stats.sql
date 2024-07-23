5b335cc79f31965c7558d5d20eacbe9a9d3935dd
Fatima Khan <fatima.k215@gmail.com>
2024-07-23 13:28:29 +0200

    bug fix

--- models/mart/mart_route_stats.sql
+++ models/mart/mart_route_stats.sql
@@ -8,7 +8,7 @@
         ad.name AS destination_airport_name,
         ad.city AS destination_city,
         ad.country AS destination_country,
-        COUNT(faa) AS total_flights,
+        COUNT(f.flight_date) AS total_flights,
         COUNT(DISTINCT f.tail_number) AS unique_airplanes,
         COUNT(DISTINCT f.airline) AS unique_airlines,
         ROUND(AVG(f.actual_elapsed_time), 2) AS avg_actual_elapsed_time,
@@ -18,11 +18,11 @@
         SUM(CASE WHEN f.cancelled = 1 THEN 1 ELSE 0 END) AS total_cancelled,
         SUM(CASE WHEN f.diverted = 1 THEN 1 ELSE 0 END) AS total_diverted
     FROM 
-        {{ref('prep_flights')}} f
+        prep_flights f
     LEFT JOIN 
-        {{ref('prep_airports')}} ao ON f.origin = ao.faa
+        prep_airports ao ON f.origin = ao.faa
     LEFT JOIN 
-        {{ref('prep_airports')}} ad ON f.dest = ad.faa
+        prep_airports ad ON f.dest = ad.faa
     GROUP BY 
         f.origin, ao.name, ao.city, ao.country,
         f.dest, ad.name, ad.city, ad.country
@@ -48,4 +48,4 @@ SELECT
 FROM 
     route_stats
 ORDER BY 
-    origin_airport_code, destination_airport_code
+    origin_airport_code, destination_airport_code;
