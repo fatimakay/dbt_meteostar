ff7f21b05a6422afc2b0ddf3225a5e3bafa14a8f
Fatima Khan <fatima.k215@gmail.com>
2024-07-23 13:22:50 +0200

    adding more marts

--- models/mart/mart_faa_stats.sql
+++ models/mart/mart_faa_stats.sql
@@ -6,16 +6,16 @@ WITH airport_stats AS (
         pa.country,
         COUNT(DISTINCT CASE WHEN pf.origin = pa.faa THEN pf.dest END) AS unique_departure_connections,
         COUNT(DISTINCT CASE WHEN pf.dest = pa.faa THEN pf.origin END) AS unique_arrival_connections,
-        COUNT(*) AS total_planned_flights,
+        COUNT(pf.flight_date) AS total_planned_flights,
         SUM(CASE WHEN pf.cancelled = 1 THEN 1 ELSE 0 END) AS total_cancelled_flights,
         SUM(CASE WHEN pf.diverted = 1 THEN 1 ELSE 0 END) AS total_diverted_flights,
         SUM(CASE WHEN pf.cancelled = 0 AND pf.diverted = 0 THEN 1 ELSE 0 END) AS total_actual_flights,
         COUNT(DISTINCT CASE WHEN pf.origin = pa.faa OR pf.dest = pa.faa THEN pf.tail_number END) AS unique_airplanes,
         COUNT(DISTINCT CASE WHEN pf.origin = pa.faa OR pf.dest = pa.faa THEN pf.airline END) AS unique_airlines
     FROM 
-        {{ref('prep_airports')}} pa
+        prep_airports pa
     LEFT JOIN 
-        {{ref('prep_flights')}} pf ON pa.faa = pf.origin OR pa.faa = pf.dest
+        prep_flights pf ON pa.faa = pf.origin OR pa.faa = pf.dest
     GROUP BY 
         pa.faa, pa.name, pa.city, pa.country
 )
@@ -35,4 +35,4 @@ SELECT
 FROM 
     airport_stats
 ORDER BY 
-    airport_code
+    airport_code;
---
+++ models/mart/mart_route_stats.sql
@@ -0,0 +1,51 @@
+   WITH route_stats AS (
+    SELECT 
+        f.origin AS origin_airport_code,
+        ao.name AS origin_airport_name,
+        ao.city AS origin_city,
+        ao.country AS origin_country,
+        f.dest AS destination_airport_code,
+        ad.name AS destination_airport_name,
+        ad.city AS destination_city,
+        ad.country AS destination_country,
+        COUNT(faa) AS total_flights,
+        COUNT(DISTINCT f.tail_number) AS unique_airplanes,
+        COUNT(DISTINCT f.airline) AS unique_airlines,
+        ROUND(AVG(f.actual_elapsed_time), 2) AS avg_actual_elapsed_time,
+        ROUND(AVG(f.arr_delay), 2) AS avg_arrival_delay,
+        MAX(f.arr_delay) AS max_delay,
+        MIN(f.arr_delay) AS min_delay,
+        SUM(CASE WHEN f.cancelled = 1 THEN 1 ELSE 0 END) AS total_cancelled,
+        SUM(CASE WHEN f.diverted = 1 THEN 1 ELSE 0 END) AS total_diverted
+    FROM 
+        prep_flights f
+    LEFT JOIN 
+        prep_airports ao ON f.origin = ao.faa
+    LEFT JOIN 
+        prep_airports ad ON f.dest = ad.faa
+    GROUP BY 
+        f.origin, ao.name, ao.city, ao.country,
+        f.dest, ad.name, ad.city, ad.country
+)
+SELECT 
+    origin_airport_code,
+    origin_airport_name,
+    origin_city,
+    origin_country,
+    destination_airport_code,
+    destination_airport_name,
+    destination_city,
+    destination_country,
+    total_flights,
+    unique_airplanes,
+    unique_airlines,
+    avg_actual_elapsed_time,
+    avg_arrival_delay,
+    max_delay,
+    min_delay,
+    total_cancelled,
+    total_diverted
+FROM 
+    route_stats
+ORDER BY 
+    origin_airport_code, destination_airport_code;
